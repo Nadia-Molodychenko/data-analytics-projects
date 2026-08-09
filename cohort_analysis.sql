f52c6daead903dbde976f9ae857088eed12de2b3
Nadia-Molodychenko <lvovanadia73@gmail.com>
2026-08-09 23:59:36 +0300

    cohort_analysis.sql

--- cohort_analysis.sql
+++ cohort_analysis.sql
@@ -1,64 +1,57 @@
-with users_parsed as(
-select lower(trim(u.email)) as email,
-       u.user_id,
-       u.signup_datetime ,
-       u.promo_signup_flag,
-case 
-	when replace(replace(
-	         split_part(
-	             trim(u.signup_datetime),' ', 1),'.','-'), '/', '-') ~ '^\d{1,2}-\d{1,2}-\d{4}$'
-    then to_date(replace(replace(split_part(trim(u.signup_datetime), ' ', 1), '.', '-'), '/', '-'), 'DD-MM-YYYY')::timestamp
-when replace(replace(
-	         split_part(
-	             trim(u.signup_datetime),' ', 1),'.','-'), '/', '-') ~ '^\d{1,2}-\d{1,2}-\d{2}$'
-    then to_date(replace(replace(split_part(trim(u.signup_datetime), ' ', 1), '.', '-'), '/', '-'), 'DD-MM-YY')::timestamp
-    else null
-end as signup_ts
-from project.cohort_users_raw u),
-events_parsed as(
-select a.user_id,
-       a.event_type,
-case 
-	when replace(replace(
-	         split_part(
-	             trim(a.event_datetime),' ', 1),'.','-'), '/', '-') ~ '^\d{1,2}-\d{1,2}-\d{4}$'
-    then to_date(replace(replace(split_part(trim(a.event_datetime), ' ', 1), '.', '-'), '/', '-'), 'DD-MM-YYYY')::timestamp
-when replace(replace(
-	         split_part(
-	             trim(a.event_datetime),' ', 1),'.','-'), '/', '-') ~ '^\d{1,2}-\d{1,2}-\d{2}$'
-    then to_date(replace(replace(split_part(trim(a.event_datetime), ' ', 1), '.', '-'), '/', '-'), 'DD-MM-YY')::timestamp
-    else null
-end as event_ts
-from project.cohort_events_raw a),
-user_activity as(
-select 
-u.user_id,
-date_trunc('month', signup_ts)::date as cohort_month,
-u.promo_signup_flag,
-date_trunc('month',event_ts)as activity_month,
-(extract('year' from a.event_ts) - extract('year' from u.signup_ts)) * 12 
-                                   +
-(extract('month' from a.event_ts) - extract('month' from u.signup_ts)) AS month_offset
-from users_parsed u left join events_parsed a
-on u.user_id = a.user_id
-where u.signup_ts is not null
-and a.event_ts is not null
-and a.event_type is not null 
-and a.event_type <> 'test_event')
-select promo_signup_flag,
-       cohort_month,
-       month_offset,
- count(distinct user_id) as users_total   
- from user_activity
-where activity_month between '2025-01-01'and '2025-06-30'
- group by promo_signup_flag,
-          cohort_month,
-          month_offset
- order by promo_signup_flag,
-          cohort_month ,
-          month_offset ;       
-          
-          
-          
-
-
+with cohort_users as (
+    select *
+    from (
+        select *,
+            to_timestamp(
+                split_part(new_signup_datetime, '-', 1) || '-' ||
+                split_part(new_signup_datetime, '-', 2) || '-' ||
+                case
+                    when length(split_part(new_signup_datetime, '-', 3)) = 2
+                    then '20' || split_part(new_signup_datetime, '-', 3)
+                    else split_part(new_signup_datetime, '-', 3)
+                end,
+                'dd-mm-yyyy'
+            ) as clean_signup_datetime
+        from (
+            select *,
+                regexp_replace(split_part(trim(signup_datetime), ' ', 1), '[./]', '-', 'g') as new_signup_datetime
+            from ads_analysis_goit_course.project.cohort_users_raw
+        ) t
+    ) t2
+    where clean_signup_datetime is not null
+),
+cohort_events as (
+    select *
+    from (
+        select *,
+            to_timestamp(
+                split_part(new_event_datetime, '-', 1) || '-' ||
+                split_part(new_event_datetime, '-', 2) || '-' ||
+                case
+                    when length(split_part(new_event_datetime, '-', 3)) = 2
+                    then '20' || split_part(new_event_datetime, '-', 3)
+                    else split_part(new_event_datetime, '-', 3)
+                end,
+                'dd-mm-yyyy'
+            ) as clean_event_datetime
+        from (
+            select *,
+                regexp_replace(split_part(trim(event_datetime), ' ', 1), '[./]', '-', 'g') as new_event_datetime
+            from ads_analysis_goit_course.project.cohort_events_raw
+        ) t
+    ) t2
+    where clean_event_datetime is not null
+      and event_type is not null
+      and event_type <> 'test_event'
+)
+select
+    promo_signup_flag,
+    date(date_trunc('month', u.clean_signup_datetime)) as cohort_month,
+    (extract(year from e.clean_event_datetime) - extract(year from u.clean_signup_datetime)) * 12
+    + (extract(month from e.clean_event_datetime) - extract(month from u.clean_signup_datetime)) as month_offset,
+    count(distinct u.user_id) as users_total
+from cohort_users u
+left join cohort_events e on u.user_id = e.user_id
+where date(date_trunc('month', e.clean_event_datetime)) between '2025-01-01' and '2025-06-01'
+group by promo_signup_flag, cohort_month, month_offset
+order by promo_signup_flag, cohort_month, month_offset
